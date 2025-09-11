fd7cf1188d57575f83a3ab6d78e816572a13ffce
Brock Schoenthaler <brockschoenthaler@gmail.com>
2025-09-11 15:16:45 -0400

    changed registration page

--- software_engineering_mobile/lib/Active_Loans.dart
+++ software_engineering_mobile/lib/Active_Loans.dart
@@ -16,12 +16,7 @@ class ActiveLoans extends StatelessWidget {
           padding: EdgeInsets.all(16.0),
           child: Column(
             children: [
-              Text(
-                'This is the New Page',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              Text('Both buttons navigate to this page.'),
+          
             ],
           ),
         ),
