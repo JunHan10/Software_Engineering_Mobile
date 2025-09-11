59e258a2cffcbc6a3429b7cc8b476d92c9bf4341
Daniel Mullinix <dlmullinix42@tntech.edu>
2025-09-11 14:45:33 -0500

    changes

--- software_engineering_mobile/lib/services/money_service.dart
+++ software_engineering_mobile/lib/services/money_service.dart
@@ -1,17 +1,17 @@
 // lib/services/money_service.dart
 import 'package:intl/intl.dart';
 
-/// Utility for formatting/parsing Hippopotamoney.
+/// Utility for formatting/parsing Hippo Bucks.
 /// We store money as integer cents to avoid floating-point errors.
 class MoneyService {
-  // H$ with 2 decimals; currency code is arbitrary here ("HPM")
+  // "HB" with 2 decimals; currency code is arbitrary ("HBUX")
   static final NumberFormat _fmt =
-  NumberFormat.currency(symbol: 'H\$', decimalDigits: 2, name: 'HPM');
+  NumberFormat.currency(symbol: 'HB ', decimalDigits: 2, name: 'HBUX');
 
-  /// Format an integer number of cents to "H$X.YY"
+  /// Format an integer number of cents to "HB X.YY"
   static String formatCents(int cents) => _fmt.format(cents / 100);
 
-  /// Parse a user-entered string like "12.34" or "H$ 12.34" to cents
+  /// Parse a user-entered string like "12.34" or "HB 12.34" to cents
   static int parseToCents(String input) {
     final cleaned = input.replaceAll(RegExp(r'[^0-9.]'), '');
     final val = double.tryParse(cleaned) ?? 0.0;
