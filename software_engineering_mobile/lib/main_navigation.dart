1423d0957e74bc16e1a0c653ec1c6cb0ba703fbc
Jun Han <fhan43@tntech.edu>
2025-09-11 14:17:45 -0500

    resolved merge conflict

--- software_engineering_mobile/lib/main_navigation.dart
+++ software_engineering_mobile/lib/main_navigation.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dashboard_page.dart';
-<<<<<<< HEAD
 import 'new_item_page.dart';
-// import 'search_page.dart';
-// import 'profile.dart';
-=======
 import 'Loaned_Items.dart';
 import 'Active_Loans.dart';
 import 'profile.dart';
->>>>>>> 1320677b7af9c34738d0d90ab7e21177271eeea9
 
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
@@ -20,8 +15,7 @@ class MainNavigation extends StatefulWidget {
 class _MainNavigationState extends State<MainNavigation> {
   int _selectedIndex = 0;
 
-<<<<<<< HEAD
-  /// List of pages to navigate (replace "center" with actual pages)
+  /// List of pages to navigate
   late List<Widget> _pages;
 
   @override
@@ -37,19 +31,10 @@ class _MainNavigationState extends State<MainNavigation> {
           });
         },
       ),
-      Center(child: Text('Messages Page', style: TextStyle(fontSize: 24))),
+      Loaned_Items(),
+      ActiveLoans(),
     ];
   }
-=======
-  /// List of pages to navigate
-  final List<Widget> _pages = [
-    DashboardPage(),
-    Loaned_Items(),
-    ActiveLoans(),
-    ProfilePage(),
-  ];
->>>>>>> 1320677b7af9c34738d0d90ab7e21177271eeea9
-
 
   void _onItemTapped(int index) {
     setState(() {
